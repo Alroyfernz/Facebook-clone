cd3d443a5e9e68feee03c9cad5422f54e8d152d7
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-09-27 00:56:59 +0530

    implemented delete and get for user restapi

--- api/routes/user.js
+++ api/routes/user.js
@@ -24,4 +24,26 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      await User.deleteOne({ _id: req.params.id });
+      res.status(200).json("Account has been deleted");
+    } catch (error) {
+      res.status(500).json("error while deleting user");
+    }
+  } else {
+    return res.status(403).json("you can only delete your account");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
